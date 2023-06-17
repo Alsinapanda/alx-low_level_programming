732d3292e5cbed8ad52ae0dd3a1ace08ec83bf9f
Alsinapanda <imrankatuba2017@gmail.com>
2023-06-17 14:17:21 -0700

    Added size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -16,6 +16,14 @@ int main(void)
 	return (0);
 }
 
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
 	printf("Size of a char: 1 byte(s)\n");
