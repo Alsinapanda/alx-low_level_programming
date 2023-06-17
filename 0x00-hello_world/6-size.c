c5a033c943df0f09b4ae05db1f75607c21143118
Alsinapanda <imrankatuba2017@gmail.com>
2023-06-17 13:13:52 -0700

    Added size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,11 +8,8 @@
 
 int main(void)
 {
-printf("Size of a char: 1 byte(s)
-	Size of an int: 4 byte(s)
-	Size of a long int: 4 byte(s)
-	Size of a long long int: 8 byte(s)
-	Size of a float: 4 byte(s)");
+printf("Size of a char: 1 byte(s)\nSize of an int: 4 byte(s)\nSize of a long int: 4 byte(s)\nSize of a long long int: 8 byte(s)\nSize of a float: 4 byte(s)");
+printf("Size of a char: 1 byte(s)\nSize of an int: 4 byte(s)\nSize of a long int: 8 byte(s)\nSize of a long long int: 8 byte(s)\nSize of a float: 4 byte(s)")
 
 return (0);
 }
