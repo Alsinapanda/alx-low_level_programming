6f2d12125d6cd96162b16c5f2aaea4efc96ccf53
Alsinapanda <imrankatuba2017@gmail.com>
2023-06-17 13:28:46 -0700

    Added size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,7 +8,9 @@
 
 int main(void)
 {
+	printf("32 bit machine\n");
 	printf("Size of a char: 1 byte(s)\nSize of an int: 4 byte(s)\nSize of a long int: 4 byte(s)\nSize of a long long int: 8 byte(s)\nSize of a float: 4 byte(s)");
+	printf("64 bit machine\n");
 	printf("Size of a char: 1 byte(s)\nSize of an int: 4 byte(s)\nSize of a long int: 8 byte(s)\nSize of a long long int: 8 byte(s)\nSize of a float: 4 byte(s)");
 
 return (0);
