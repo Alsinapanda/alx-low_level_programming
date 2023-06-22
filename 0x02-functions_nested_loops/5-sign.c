c3d20d85bc8daff27446eb33dc92c0cdbdbe32ca
Alsinapanda <imrankatuba2017@gmail.com>
2023-06-22 12:15:22 -0700

    Added Sign file

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,7 +3,7 @@
 /**
  * print_sign - Prints the sign of a number
  * @n: The number to be checked
- * Retun: 1 - for positive number -1 - for negative number 0 - Anythin else
+ * Return: 1 - for positive number, -1 - for negative number, 0 - Anything else
  */
 
 int print_sign(int n)
